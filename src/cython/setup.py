8a8096b4fe2fcd4845810fb1e1e0a99b0ce6b8dc
Artprozew <artprozew@gmail.com>
2024-05-29 02:52:02 -0300

    Refactor: small build changes

--- src/cython/setup.py
+++ src/cython/setup.py
@@ -5,5 +5,6 @@ setup(
     ext_modules=cythonize(Extension(
         "cython_prime_counter",
         sources=["cython_prime_counter.pyx"],
-    ))
+    ),
+    language_level=3)
 ) # FIXME something ocasionally returns exit code -1073740791 ?
